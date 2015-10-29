af783b1c59aa8dd5217e8bbeb593eac0c3dbdbb2
iveszhou <68079320@qq.com>
2015-10-29 12:13:08 +0800

    优化显示页面，加入github链接。

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php require 'lib/Parsedown.class.php'; ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>golang web编程！</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <div class="container">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>golang web编程！</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+  <div class="col-sm-12 col-lg-12 col-md-12">
     <?php
     if(isset($_GET['file'])){
 		$file = $_GET['file'];
 	} else {
-		$file = '01.0.md';
+		$file = 'preface.md';
 	}
 	$file = str_replace('<', '', $file);
 	$file = str_replace('>', '', $file);
@@ -22,6 +23,14 @@
 	$Parsedown = new Parsedown();
 	echo $Parsedown->text($content); # prints: <p>Hello <em>Parsedown</em>!</p>
 ?>
-    </div><!-- /.container -->
-  </body>
+  </div>
+  <div class="col-sm-12 col-lg-12 col-md-12">
+    <footer>
+	<hr>
+	Go Web编程的github地址为：https://github.com/astaxie/build-web-application-with-golang<br />
+	本项目的github地址为：https://github.com/iveszhou/PHP-For-Go-Web<br />
+	</footer>
+  </div>
+</div><!-- /.container -->
+</body>
 </html>
