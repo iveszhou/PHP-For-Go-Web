b0faae325e4056c03c2d84a56512519dee2073de
iveszhou <68079320@qq.com>
2015-11-12 09:56:34 +0800

    优化title标签。 Signed-off-by: iveszhou <68079320@qq.com>

--- index.php
+++ index.php
@@ -12,6 +12,18 @@ if(file_exists($file)){
 } else {
 	header('HTTP/1.1 404 Not Found');
 }
+$con = '';
+$title = '';
+if(empty($content)){
+	$title = '404错误，页面不存在！';
+} else {
+	$Parsedown = new Parsedown();
+	$con = $Parsedown->text($content); # prints: <p>Hello <em>Parsedown</em>!</p>
+	preg_match('/^\<h1\>(.*?)\<\/h1\>/', $con,$matches);
+	if(isset($matches[1])){
+		$title = $matches[1];
+	}
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,28 +32,27 @@ if(file_exists($file)){
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>golang web编程！</title>
+  <title><?php echo $title?$title:'golang web编程！'; ?></title>
   <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
   <div class="col-sm-12 col-lg-12 col-md-12">
 <?php
-if(empty($content)){
-	echo '<h1>页面不存在！</h1>';
+if(empty($con)){
+	echo '<p>页面不存在！</p>';
 } else {
-	$Parsedown = new Parsedown();
-	echo $Parsedown->text($content); # prints: <p>Hello <em>Parsedown</em>!</p>
+	echo $con;
 }
 ?>
   </div>
   <div class="col-sm-12 col-lg-12 col-md-12">
     <footer>
 	<hr>
-	Go Web编程的github地址为：https://github.com/astaxie/build-web-application-with-golang<br />
-	本项目的github地址为：https://github.com/iveszhou/PHP-For-Go-Web<br />
+	Go Web编程的github地址为：<a href="https://github.com/astaxie/build-web-application-with-golang" target="_blank">https://github.com/astaxie/build-web-application-with-golang</a><br />
+	本项目的github地址为：<a href="https://github.com/iveszhou/PHP-For-Go-Web" target="_blank">https://github.com/iveszhou/PHP-For-Go-Web</a><br />
 	</footer>
   </div>
-</div><!-- /.container -->
+</div>
 </body>
 </html>
